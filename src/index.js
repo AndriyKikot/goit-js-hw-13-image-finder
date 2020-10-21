f9a9c044b07994b4882cbe5a35aeb3b93b9b174d
Andriy Kikot <andray838@gmail.com>
2020-10-21 23:50:12 +0300

    added almost finished homework

--- src/index.js
+++ src/index.js
@@ -2,41 +2,33 @@ import './styles.css';
 import './scss/normalize.scss';
 import 'material-design-icons/iconfont/material-icons.css';
 
-import imagesListTemplate from './templates/images-list.hbs'
-
 import apiService from './js/apiService.js';
+import updateGalleryMarkup from './js/updateGalleryMarkup';
 
 import { refs } from './js/refs.js';
-
 
 
 refs.searchForm.addEventListener('submit', event => {
     event.preventDefault();
 
     const form = event.currentTarget;
-    const inputValue = form.elements.query.value;
-    console.log(inputValue);
+    apiService.query = form.elements.query.value;
 
     refs.galleryList.innerHTML = '';
+    form.reset();
 
-    apiService(inputValue);
-    function apiService(searchQuery) {
-        const apiKey = '18773042-c85a376c8239f0d185771db9c';
-        const url = ` https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${apiKey}`;
+    apiService.resetPage();
 
-        fetch(url)
-            .then(res => res.json())
-            .then(({ hits }) => {
-                console.log(hits);
+    apiService.fetchImages().then(hits => {
+        updateGalleryMarkup(hits);
+    });
 
-                const murkup = imagesListTemplate(hits)
+});
 
-                refs.galleryList.insertAdjacentHTML('beforeend', murkup);
-
-            })
-            .catch(error => console.log(error));
-    }
-})
+refs.loadMoreBtn.addEventListener('click', () => {
+    apiService.fetchImages().then(hits => {
+        updateGalleryMarkup(hits);
+    });
 
 
-
+});
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
     galleryList: document.querySelector('.gallery'),
     searchForm: document.querySelector('.search-form'),
+    loadMoreBtn: document.querySelector('.load-more__button'),
 };
