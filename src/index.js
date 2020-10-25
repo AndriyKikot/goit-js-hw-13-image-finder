80c609c049d37f36685ebb7b038bcbefdcb5221d
Andriy Kikot <andray838@gmail.com>
2020-10-25 22:41:30 +0300

    added basicLightbox

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './styles.css';
 import './scss/normalize.scss';
 import 'material-design-icons/iconfont/material-icons.css';
+import './scss/basicLightBox.scss';
 
 import updateGalleryMarkup from './js/updateGalleryMarkup';
 
 import { refs } from './js/refs.js';
 import apiService from './js/apiService.js';
 import loadMoreBtn from './js/loadMoreBtn.js';
-import error from './js/pnotify.js'
+import error from './js/pnotify.js';
+import createModal from './js/basicLightbox.js';
 
 
 refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
 refs.loadMoreBtn.addEventListener('click', fetchImages);
+refs.galleryList.addEventListener('click', hadleOpenModal);
+
 
 function searchFormSubmitHandler(event) {
     event.preventDefault();
@@ -24,6 +28,7 @@ function searchFormSubmitHandler(event) {
     fetchImages();
     form.reset();
 };
+
 function fetchImages() {
 
     loadMoreBtn.disable();
@@ -43,6 +48,16 @@ function fetchImages() {
         }
     }).catch(error => (console.log(error)));
 };
+
 function clearGalleryList() {
     refs.galleryList.innerHTML = '';
 };
+
+function hadleOpenModal(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const largeImageURL = event.target.dataset.source;
+
+    createModal(largeImageURL);
+}
